24992555fb872eec47c133ce56abbd54e25a0249
tom <ken61ab@gmail.com>
2018-07-07 17:51:42 +0800

    新增 middleware - FormInputSingleSpace

---
+++ app/Http/Middleware/FormInputSingleSpace.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class FormInputSingleSpace
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @param  params  $inputsName
+     * @return mixed
+     */
+    public function handle($request, Closure $next, ...$inputsName)
+    {
+        $inputs = $request->only($inputsName);
+
+        foreach ($inputs as $key => $val)
+        {
+            $request->merge([$key => single_space($val)]);
+        }
+
+        return $next($request);
+    }
+}
